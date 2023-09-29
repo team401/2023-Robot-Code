96fa9a1ea70ce7e0eb0cf3a0c0aadd166d0bb45c
fujibookpro <maczone06@gmail.com>
2023-09-29 11:49:28 -0400

    Update paths, change auto selector options

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,7 +79,6 @@ public class RobotContainer {
     }
 
     private void configureSubsystems() {
-
         drive.setDefaultCommand(new DriveWithJoysticks(
             drive,
             () -> -leftStick.getRawAxis(1),
@@ -91,7 +90,6 @@ public class RobotContainer {
         pivot.setDefaultCommand(new HoldPivot(pivot, telescope));
         telescope.setDefaultCommand(new HoldTelescope(telescope, pivot));
         wrist.setDefaultCommand(new HoldWrist(wrist, pivot));
-
     }
 
     @SuppressWarnings("unused")
@@ -219,28 +217,27 @@ public class RobotContainer {
 
         masher.start()
             .onTrue(new InstantCommand(() -> RobotState.getInstance().invertBack()));
-        }
+    }
 
     private void configureAutos() {
-        autoChooser.addOption("B-1-1", "B-1-1");
-        autoChooser.addOption("B-1-2", "B-1-2");
-        autoChooser.addOption("B-1-3", "B-1-3");
+        autoChooser.setDefaultOption("Blue upper 3", "B-1-1");
+        autoChooser.addOption("Blue upper 2.5", "B-1-2");
+        // autoChooser.addOption("B-1-3", "B-1-3");
 
-        autoChooser.addOption("B-2-1", "B-2-1");
+        autoChooser.addOption("Blue middle balance", "B-2-1");
 
-        autoChooser.addOption("B-3-1", "B-3-1");
-        autoChooser.addOption("B-3-2", "B-3-2");
+        // autoChooser.addOption("B-3-1", "B-3-1");
+        // autoChooser.addOption("B-3-2", "B-3-2");
 
-        autoChooser.addOption("R-1-1", "R-1-1");
-        autoChooser.addOption("R-1-2", "R-1-2");
-        autoChooser.addOption("R-1-3", "R-1-3");
+        autoChooser.addOption("Red upper 3", "R-1-1");
+        autoChooser.addOption("Red upper 2.5", "R-1-2");
+        // autoChooser.addOption("R-1-3", "R-1-3");
 
-        autoChooser.addOption("R-2-1", "R-2-1");
+        autoChooser.addOption("Red middle balance", "R-2-1");
 
-        autoChooser.addOption("R-3-1", "R-3-1");
-        autoChooser.addOption("R-3-2", "R-3-2");
-        
-        autoChooser.setDefaultOption("default", "B-1-1");
+        // autoChooser.addOption("R-3-1", "R-3-1");
+        // autoChooser.addOption("R-3-2", "R-3-2");
+
         SmartDashboard.putData("Auto Mode", autoChooser);
     }
 
@@ -260,11 +257,9 @@ public class RobotContainer {
         wrist.setBrakeMode(!brakeSwitch.get());
 
         ledManager.setOff(ledsSwitch.get());
-
     }
 
     public void enabledInit() {
-
         drive.setBrakeMode(true);
         pivot.setBrakeMode(true);
         telescope.setBrakeMode(true);
